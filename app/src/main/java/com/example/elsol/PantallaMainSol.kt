b1afbda04c01d89c9f82de7b008c00f50dfb2ae6
Darius <dartig@alu.edu.gva.es>
2025-11-03 20:05:08 +0100

    LazyVerticalGrid con los Cards terminado

--- app/src/main/java/com/example/elsol/PantallaMainSol.kt
+++ app/src/main/java/com/example/elsol/PantallaMainSol.kt
@@ -1,26 +1,89 @@
 package com.example.elsol
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun PantallaMainSol(modifier: Modifier) {
     Column(
-        modifier = Modifier
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
             .fillMaxSize()
     ) {
-
+        LazyVerticalGridFotosSol()
     }
 }
 
 @Composable
-fun StaggeredGridFotosSol() {
+fun LazyVerticalGridFotosSol() {
+    val listaFotos = getImagenesSol()
 
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        contentPadding = PaddingValues(12.dp),
+        content = {
+            items(listaFotos.size) { i ->
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(3.dp)
+                ) {
+                    Column {
+                        Image(
+                            painter = painterResource(listaFotos[i].rutaImagen),
+                            contentDescription = listaFotos[i].descripImagen,
+                            contentScale = ContentScale.Crop,
+                            modifier = Modifier
+                                .height(200.dp)
+                        )
+                        Row(
+                            verticalAlignment = Alignment.CenterVertically,
+                            horizontalArrangement = Arrangement.SpaceBetween,
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .background(Color(228, 218, 253, 255))
+                                .padding(start = 12.dp, end = 12.dp)
+                        ) {
+                            Text(
+                                text = listaFotos[i].textoImagen
+                            )
+                            IconButton(
+                                onClick = { /* Drop down menu XDXDXD */ }
+                            ) {
+                                Icon(
+                                    Icons.Default.MoreVert,
+                                    "Icono tres puntos dropdown"
+                                )
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    )
 }
 
 fun getImagenesSol(): List<ImagenSol> {
