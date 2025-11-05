246369861673ae3ebe086b92867fddd7712d02e7
Darius <dartig@alu.edu.gva.es>
2025-11-05 11:46:34 +0100

    Cajon Lateral terminado

--- app/src/main/java/com/example/elsol/MainActivity.kt
+++ app/src/main/java/com/example/elsol/MainActivity.kt
@@ -5,13 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.Add
@@ -22,16 +24,19 @@ import androidx.compose.material.icons.filled.Info
 import androidx.compose.material3.Badge
 import androidx.compose.material3.BadgedBox
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FilledTonalButton
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalNavigationDrawer
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
@@ -40,13 +45,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.elsol.ui.theme.ElSolTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -60,77 +68,88 @@ class MainActivity : ComponentActivity() {
                 val snackbarHostState = remember { SnackbarHostState() }
                 val corutina = rememberCoroutineScope()
                 val navController = rememberNavController()
+                val drawerState = rememberDrawerState(initialValue = DrawerValue.Closed)
 
-                Scaffold(
-                    snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
-                    modifier = Modifier.fillMaxSize(),
-                    bottomBar = {
-                        BottomAppBar(
-                            containerColor = Color.Red,
-                            contentColor = Color.White,
-                            actions = {
-                                IconButton(
-                                    onClick = {
-
-                                    }
-                                ) {
-                                    Icon(
-                                        Icons.AutoMirrored.Default.ArrowBack,
-                                        "Icono flecha izquierda"
-                                        )
-                                }
-                                BadgedBox(
-                                    badge = {
-                                        if (itemCount > 0) {
-                                            Badge(
-                                                containerColor = Color(174, 35, 30, 255),
-                                                contentColor = Color.White
-                                            ) {
-                                                Text(
-                                                    text = "$itemCount"
-                                                )
+                ModalNavigationDrawer(
+                    modifier = Modifier
+                        .padding(top = 20.dp),
+                    drawerState = drawerState,
+                    drawerContent = {
+                        CajonLateral(getInfoBotones())
+                    }
+                ) {
+                    Scaffold(
+                        snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
+                        modifier = Modifier.fillMaxSize(),
+                        bottomBar = {
+                            BottomAppBar(
+                                containerColor = Color.Red,
+                                contentColor = Color.White,
+                                actions = {
+                                    IconButton(
+                                        onClick = {
+                                            corutina.launch {
+                                                drawerState.open()
                                             }
                                         }
-                                    }
-                                ) {
-                                    IconButton(
-                                        onClick = { itemCount++ }
                                     ) {
                                         Icon(
-                                            Icons.Default.Favorite,
-                                            "Icono corazon izquierda"
+                                            Icons.AutoMirrored.Default.ArrowBack,
+                                            "Icono flecha izquierda"
+                                        )
+                                    }
+                                    BadgedBox(
+                                        badge = {
+                                            if (itemCount > 0) {
+                                                Badge(
+                                                    containerColor = Color(174, 35, 30, 255),
+                                                    contentColor = Color.White
+                                                ) {
+                                                    Text(
+                                                        text = "$itemCount"
+                                                    )
+                                                }
+                                            }
+                                        }
+                                    ) {
+                                        IconButton(
+                                            onClick = { itemCount++ }
+                                        ) {
+                                            Icon(
+                                                Icons.Default.Favorite,
+                                                "Icono corazon izquierda"
+                                            )
+                                        }
+                                    }
+                                },
+                                floatingActionButton = {
+                                    FloatingActionButton(
+                                        onClick = {/* Accion del FAB */},
+                                        containerColor = MaterialTheme.colorScheme.primaryContainer,
+                                        contentColor = Color(0, 0, 0)
+                                    ) {
+                                        Icon(
+                                            imageVector = Icons.Filled.Add,
+                                            contentDescription = "FAB añadir"
                                         )
                                     }
                                 }
-                            },
-                            floatingActionButton = {
-                                FloatingActionButton(
-                                    onClick = {/* Accion del FAB */},
-                                    containerColor = MaterialTheme.colorScheme.primaryContainer,
-                                    contentColor = Color(0, 0, 0)
-                                ) {
-                                    Icon(
-                                        imageVector = Icons.Filled.Add,
-                                        contentDescription = "FAB añadir"
-                                    )
-                                }
-                            }
-                        )
-                        //CajonLateral()
-                    }
+                            )
+                        }
                     ) { innerPadding ->
-                    NavHost(
-                        navController = navController,
-                        startDestination = "PantallaMainSol",
-                        modifier = Modifier
-                            .padding(innerPadding)
-                    ) {
-                        composable("PantallaMainSol") {
-                            PantallaMainSol(
-                                modifier = Modifier,
-                                snackbarHostState = snackbarHostState,
-                                coroutineScope = corutina
+                        NavHost(
+                            navController = navController,
+                            startDestination = "PantallaMainSol",
+                            modifier = Modifier
+                                .padding(innerPadding)
+                        ) {
+                            composable("PantallaMainSol") {
+                                PantallaMainSol(
+                                    modifier = Modifier,
+                                    snackbarHostState = snackbarHostState,
+                                    coroutineScope = corutina
                                 )
+                            }
                         }
                     }
                 }
@@ -158,35 +177,36 @@ fun getInfoBotones(): List<BotonesCajon> {
 
 @Composable
 fun CajonLateral(infoBotones: List<BotonesCajon>) {
-    Box {
-        Image(
-            painter = painterResource(R.drawable.erupcionsolar),
-            contentDescription = "imagen cajon",
-            modifier = Modifier
-                .fillMaxWidth()
-        )
-    }
-    Column(
+    Box(
         modifier = Modifier
-            .padding(8.dp)
+            .clip(RoundedCornerShape(topEnd = 10.dp))
+            .fillMaxHeight()
+            .background(MaterialTheme.colorScheme.primaryContainer),
     ) {
-        infoBotones.forEach { boton ->
-            FilledTonalButton(
-                onClick = {  }
-            ) {
-                Row(
-                    horizontalArrangement = Arrangement.spacedBy(10.dp),
-                    verticalAlignment = Alignment.CenterVertically
-                ) {
-                    Icon(
-                        boton.iconoBoton,
-                        "iconoBoton"
-                    )
-                    Text(
-                        boton.textoBoton
-                    )
+        Column {
+            Image(
+                painter = painterResource(R.drawable.erupcionsolar),
+                contentDescription = "imagen cajon",
+                contentScale = ContentScale.Crop,
+            )
+                infoBotones.forEach { boton ->
+                    FilledTonalButton(
+                        onClick = {  }
+                    ) {
+                        Row(
+                            horizontalArrangement = Arrangement.spacedBy(10.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Icon(
+                                boton.iconoBoton,
+                                "iconoBoton"
+                            )
+                            Text(
+                                boton.textoBoton
+                            )
+                        }
+                    }
                 }
-            }
         }
     }
 }
