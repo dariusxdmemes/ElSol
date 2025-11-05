5b6b25f68b5e08e446f508f91273542d33411b3e
Darius <dartig@alu.edu.gva.es>
2025-11-05 17:26:35 +0100

    PantallaInfo.kt organizada, boton con cosito que gira y boton select date.

--- app/src/main/java/com/example/elsol/PantallaInfo.kt
+++ app/src/main/java/com/example/elsol/PantallaInfo.kt
@@ -1,14 +1,74 @@
 package com.example.elsol
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun PantallaInfo(modifier: Modifier) {
-    Column {
-        Text("HOLA")
+fun PantallaInfo() {
+
+    var loading by remember {
+        mutableStateOf(false)
     }
 
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(50, 100, 160)),
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(30.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier
+                .background(Color(200, 200, 100)),
+        ) {
+            Button(
+                onClick = {
+                    loading = true
+                }
+            ) {
+                Text(
+                    text = "Download more info"
+                )
+            }
+
+            if (!loading) return
+            CositoQueGira()
+
+            Button(
+                onClick = {/* date picker */}
+            ) {
+                Text(
+                    text = "Visit planetarium. Select date"
+                )
+            }
+        }
+    }
+}
+
+@Composable
+fun CositoQueGira() {
+    CircularProgressIndicator(
+        modifier = Modifier
+            .width(64.dp),
+        color = MaterialTheme.colorScheme.secondary,
+        trackColor = MaterialTheme.colorScheme.surfaceVariant
+    )
 }
